78acb84e683b658798d5f100f866df0aa2527847
finntenzor <finntenzor@gmail.com>
2018-12-01 10:20:01 +0800

    添加php头，修改代码格式

--- test.php
+++ test.php
@@ -1,13 +1,15 @@
- class Test {
-	private $key;
-	private $value;
+<?php
+class Test {
+    private $key;
+    private $value;
 
-	public function setKey($key) {
-		if($key > 0)
-			$this->key = $key;
-	}
- 
-	public function getKey() {
-		return $this->key;
-	}
- }
+    public function setKey($key) {
+        if ($key > 0) {
+            $this->key = $key;
+        }
+    }
+  
+    public function getKey() {
+        return $this->key;
+    }
+}
